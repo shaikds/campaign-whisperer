ff7d1c0278799b511e64ace2a1edc14f79d844ae
shaikds <shai6661@gmail.com>
2025-11-05 11:04:33 +0200

    Fix HomePage import path declaration error

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import HomePage from '../Pages/HomePage.jsx'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Analytics/> 
+    <SpeedInsights/>
     <HomePage />
   </React.StrictMode>,
 )
